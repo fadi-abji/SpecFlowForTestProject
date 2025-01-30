b0e2764a92a842ce9cba35e95455500858de72ff
Fadi Abji <fadi@wexer.se>
2025-01-30 10:19:52 +0100

    Add project files.

---
+++ SpecFlowProject1/StepDefinitions/CalculatorStepDefinitions.cs
@@ -0,0 +1,45 @@
+namespace SpecFlowProject1.StepDefinitions
+{
+    [Binding]
+    public sealed class CalculatorStepDefinitions
+    {
+        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+        [Given("the first number is (.*)")]
+        public void GivenTheFirstNumberIs(string str)
+        {
+            //TODO: implement arrange (precondition) logic
+            // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata
+            // To use the multiline text or the table argument of the scenario,
+            // additional string/Table parameters can be defined on the step definition
+            // method. 
+            Console.WriteLine($"string: {str}");
+            //throw new PendingStepException();
+        }
+
+        [Given("the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int number)
+        {
+            //TODO: implement arrange (precondition) logic
+            Console.WriteLine($"Second Number: {number}");
+            //throw new PendingStepException();
+        }
+
+        [When("the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            //TODO: implement act (action) logic
+            Console.WriteLine("When numbers are added!");
+            //throw new PendingStepException();
+        }
+
+        [Then("the result should be (.*)")]
+        public void ThenTheResultShouldBe(int result)
+        {
+            //TODO: implement assert (verification) logic
+
+            Console.WriteLine($"Result : {result}");
+            //throw new PendingStepException();
+        }
+    }
+}
